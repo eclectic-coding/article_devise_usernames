59ad85d0e034fcf735f208d134780813bbce294e
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-02 15:28:22 -0500

    Set up devise and user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'users/show'
+  get 'user/show'
+  devise_for :users
+
+  resources :users, only: [:show]
+
 end
